93dd68652861e718064109678a73eb470e8d64cf
Ragib Morshed <rmorshed@amazon.com>
2024-08-09 18:14:17 -0700

    saving

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# multi-modal-llm
+# Multi-modal-llm
+
+- Github with slides: https://github.com/hkproj/pytorch-paligemma
+- Video: https://youtu.be/vAmKB7iPkWw
+
---
+++ siglip.py
@@ -0,0 +1,92 @@
+from typing import Optional, Tuple
+import torch
+import torch.nn as nn
+
+
+class SiglipVisionConfig:
+    def __init__(
+        self,
+        hidden_size=768,
+        intermediate_size=3072,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        num_channels=3,
+        image_size=224,
+        patch_size=16,
+        layer_norm_eps=1e-6,
+        attention_dropout=0.0,
+        num_image_tokens: int = None
+    ):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_chennels = num_channels
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.layer_norm_eps = layer_norm_eps
+        self.attention_dropout = attention_dropout
+        self.num_image_tokens = num_image_tokens
+
+
+class SiglipVisionEmbeddings(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.image_size = config.image_size
+        self.patch_size = config.patch_size
+
+        self.patch_embedding = nn.Conv2d(
+            in_channels=config.num_channels,
+            out_channels=self.embed_dim,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
+            padding='valid'
+        )
+
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.num_positions = self.num_patches
+        self.position_embedding = nn.Embedding(
+            num_embeddings=self.num_positions, embedding_dim=self.embed_dim)
+        self.register_buffer(
+            "position_ids",
+            torch.arange(self.num_positions).expand((1, -1)),
+            persistent=False,
+        )
+
+    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
+        pass
+
+
+class SiglipEncoder(nn.Module):
+    pass
+
+
+class SiglipVisionTransformer(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        embed_dim = config.hidden_size
+        self.embeddings = SiglipVisionEmbeddings(config)
+        self.encoder = SiglipEncoder(config)
+        self.post_layer_norm = nn.LayerNorm(
+            embed_dim, eps=config.layer_norm_eps)
+
+    def forward(self, pixel_values: torch.Tensor) -> Tuple:
+        # (batch_size, channels, height, width) -> (batch_size, num_patches, embed_dim)
+        hidden_states = self.embeddings(pixel_values)
+        last_hidden_state = self.encoder(inputs_embeds=hidden_states)
+        last_hidden_state = self.post_layer_norm(last_hidden_state)
+        return last_hidden_state
+
+class SiglipVisionModel(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        self.vision_model = SiglipVisionTransformer(config)
+
+    def forward(self, pixel_values: torch.Tensor) -> Tuple:
+        # (batch_size, channels, height, width) -> (batch_size, num_patches, embed_dim)
+        return self.vision_model(pixel_values=pixel_values)
