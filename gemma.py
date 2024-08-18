e7a5a19cc35b3e467f7beca564383b53e69b8f25
Ragib Morshed <12161212+mordr@users.noreply.github.com>
2024-08-18 11:28:29 -0700

    RMS layer norm

--- gemma.py
+++ gemma.py
@@ -95,8 +95,18 @@ class GemmaDecoderLayer(nn.Module):
 
 
 class GemmaRMSNorm(nn.Module):
-    pass
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.zeros(dim))
 
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float())
+        output = output * (1.0 + self.weight.float())
+        return output.type_as(x)
 
 class GemmaModel(nn.Module):
     def __init__(self, config: GemmaConfig):
